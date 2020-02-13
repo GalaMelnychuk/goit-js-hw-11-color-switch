2ce5ce6539f69a285392b0b5f19035a5573c6c0e
Галина <galocka@MacBook-Galina.local>
2020-02-14 00:34:33 +0200

    pretire

--- task.js
+++ task.js
@@ -1,38 +1,39 @@
+"use strict";
 // Напиши скрипт, который после нажатия кнопки Start, раз в секунду меняет цвет фона body на случайное значение из массива используя инлайн-стиль. При нажатии на кнопку Stop, изменение цвета фона должно останавливаться.
 
 let timerId;
 
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548"
+];
 
 const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
- const refs = {
-    btnStart: document.querySelector('[data-action="start"]'),
-    btnStop: document.querySelector('[data-action="stop"]'),
-    body: document.querySelector('body'),
- };
-
-function changeColor () {
-    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 6)];
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-function hendlerClick (event) {
-    timerId = setInterval(changeColor, 1000);
+const refs = {
+  btnStart: document.querySelector('[data-action="start"]'),
+  btnStop: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector("body")
 };
 
-function hendlerStop (event) {
-    refs.btnStart.removeEventListener('click', hendlerClick);
-    clearInterval(timerId);
-};
+function changeColor() {
+  refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 6)];
+}
 
-refs.btnStart.addEventListener('click', hendlerClick);
-refs.btnStop.addEventListener('click', hendlerStop);
+function hendlerClick(event) {
+  timerId = setInterval(changeColor, 1000);
+}
+
+function hendlerStop(event) {
+  refs.btnStart.removeEventListener("click", hendlerClick);
+  clearInterval(timerId);
+}
+
+refs.btnStart.addEventListener("click", hendlerClick);
+refs.btnStop.addEventListener("click", hendlerStop);
